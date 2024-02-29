b176127c3a86e6f1c6c0663dd99a3c11e348e539
Laura De Koning <lade-kon@f0r1s13.codam.nl>
2024-02-29 17:51:43 +0100

    I think I removed all the git submodules

--- so_long.c
+++ so_long.c
@@ -6,15 +6,18 @@
 /*   By: lade-kon <lade-kon@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/23 17:43:41 by lade-kon      #+#    #+#                 */
-/*   Updated: 2024/02/23 18:11:23 by lade-kon      ########   odam.nl         */
+/*   Updated: 2024/02/29 15:16:55 by lade-kon      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/incl/libft.h"
-#include "MLX42/MLX42.h"
+#include "incl/so_long.h"
+#include "lib/libft/incl/libft.h"
+#include "lib/MLX42/include/MLX42/MLX42.h"
 
 int	main(int argc, char **argv)
 {
+
+	
 
 	return (0);
 }
